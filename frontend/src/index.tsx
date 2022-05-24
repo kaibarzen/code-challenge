911c07b63929410b7e1692d5f382f6b6b4f5de71
Kai Barzen <52884225+kaibarzen@users.noreply.github.com>
2022-05-24 16:42:53 +0200

    feat: basic setup frontend

---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'antd/dist/antd.dark.css';
+import "./index.sass"
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    Test
+  </React.StrictMode>
+);
